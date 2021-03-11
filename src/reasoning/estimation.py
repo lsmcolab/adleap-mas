11a32b796baaf89c4f4febbfc58511d9877b657b
Micanga <matheus.moog@gmail.com>
2021-03-11 10:44:47 -0300

    Adding the visibility parameter.

--- src/reasoning/estimation.py
+++ src/reasoning/estimation.py
@@ -1,18 +1,20 @@
 import random as rd
 
 def level_foraging_uniform_estimation(env):
-    for agent in env.components['agents']:
-        if agent != env.get_adhoc_agent() and\
-         None in [agent.level,agent.radius,agent.angle,agent.type]:
-            agent.level = rd.uniform(0,1)
-            agent.radius = rd.uniform(0,1)
-            agent.angle = rd.uniform(0,1)
-            agent.type = rd.sample(['l1','l2','l3','l4'],1)[0]
+    if env.visibility == 'partial':
+        for agent in env.components['agents']:
+            if agent != env.get_adhoc_agent() and\
+            None in [agent.level,agent.radius,agent.angle,agent.type]:
+                agent.level = rd.uniform(0,1)
+                agent.radius = rd.uniform(0,1)
+                agent.angle = rd.uniform(0,1)
+                agent.type = rd.sample(['l1','l2','l3','l4'],1)[0]
         
-    for task in env.components['tasks']:
-        if task.level is None:
-            task.level = rd.uniform(0,1)
-
+        for task in env.components['tasks']:
+            if task.level is None:
+                task.level = rd.uniform(0,1)
+                
+    env.visibility = 'full'
     return env
 
 def truco_uniform_estimation(env):
@@ -23,6 +25,6 @@ def truco_uniform_estimation(env):
                 while len(player.hand) < 3:
                     player.hand.append(env.components['cards in game'].pop(0))
                 player.type = rd.sample(['t1','t2','t3'],1)[0]
-                
+
     env.visibility = 'full'
     return env
