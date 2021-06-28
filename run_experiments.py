e3e830a10abc12173851d459dd25c64a7d783850
Micanga <matheus.moog@gmail.com>
2021-06-28 10:46:30 -0300

    updating experiment script

--- run_experiments.py
+++ run_experiments.py
@@ -11,37 +11,37 @@ def get_python_cmd(env, estimation, num_agents, num_tasks, dim, num_exp, num_epi
      ' --env ' + str(env) + ' --estimation ' + str(estimation) + \
      ' --num_agents ' + str(num_agents) + ' --num_tasks ' + str(num_tasks) + \
      ' --dim ' + str(dim) + ' --num_exp ' + str(num_exp) + ' --num_episodes ' + str(num_episodes) + \
-     ' --po False' 
+     ' --po False --display False' 
 
 ###
 # SCRIPT
 ###
-num_exp = 20
+num_exp = 100
 num_episodes = 200
 for experiment_id in range(num_exp):
     for num_agents in [7]:#5,7,10
         for num_tasks in [20]:#20,25,30
             for dim in [20]:#20,25,30
                 for estimation in ['AGA','ABU',"OEATA"]:
-                        # defining the experiment parameters
-                        python_cmd = get_python_cmd('LevelForagingEnv',estimation,num_agents,
-                                    num_tasks,dim,experiment_id,num_episodes)
+                    # defining the experiment parameters
+                    python_cmd = get_python_cmd('CaptureEnv',estimation,num_agents,
+                                num_tasks,dim,experiment_id,num_episodes)
 
-                        # writing the bash file                     
-                        with open("run.sh", "w") as bashfile:           
-                            bashfile.write("#$ -S /bin/bash\n\n")      
-                            bashfile.write("#$ -l h_vmem=4G\n")         
-                            bashfile.write("#$ -l h_rt=00:10:00\n\n")  
-                            bashfile.write("source /etc/profile\n")     
-                            bashfile.write("module add anaconda3/wmlce\n")                              
-                            bashfile.write("source activate wmlce_env\n\n")                             
-                            bashfile.write(python_cmd)
+                    # writing the bash file                     
+                    with open("run.sh", "w") as bashfile:           
+                        bashfile.write("#$ -S /bin/bash\n\n")      
+                        bashfile.write("#$ -l h_vmem=4G\n")         
+                        bashfile.write("#$ -l h_rt=00:10:00\n\n")  
+                        bashfile.write("source /etc/profile\n")     
+                        bashfile.write("module add anaconda3/wmlce\n")                              
+                        bashfile.write("source activate wmlce_env\n\n")                             
+                        bashfile.write(python_cmd)
 
-                        time.sleep(0.25)
+                    time.sleep(0.25)
 
-                        # submiting the job
-                        subprocess.run(["qsub",
-                         "-o","qsuboutput/"+ str(estimation)+"a"+str(num_agents)+"i"+str(num_tasks)+"d"+str(dim)+"e"+str(experiment_id)+".txt",
-                         "-e","qsuberror/"+str(estimation)+"a"+str(num_agents)+"i"+str(num_tasks)+"d"+str(dim)+"e"+str(experiment_id)+".txt",
-                         "run.sh"])
-                        time.sleep(0.25)
+                    # submiting the job
+                    subprocess.run(["qsub",
+                        "-o","qsuboutput/"+ str(estimation)+"a"+str(num_agents)+"i"+str(num_tasks)+"d"+str(dim)+"e"+str(experiment_id)+"output.txt",
+                        "-e","qsuberror/"+str(estimation)+"a"+str(num_agents)+"i"+str(num_tasks)+"d"+str(dim)+"e"+str(experiment_id)+"error.txt",
+                        "run.sh"])
+                    time.sleep(0.25)
