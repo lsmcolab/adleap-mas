81109cc7c4f23d5118c9e1ed1f5c80971211c0ae
Micanga <matheus.moog@gmail.com>
2021-07-14 17:00:55 -0300

    enabling mode argument for c1 and c2 types -- spatial and index task reasoning

--- src/reasoning/c1.py
+++ src/reasoning/c1.py
@@ -2,8 +2,8 @@ from a_star import a_star_planning
 import numpy as np
 import random
 
-# Pursues odd index preys
-def c1_planning(env,agent):
+# Pursues odd index/furthest preys
+def c1_planning(env,agent,mode='spatial'):
     action, target_pos = None, None
     
     # checking if the agent already chosen a prey
@@ -12,7 +12,7 @@ def c1_planning(env,agent):
             if(t.index == agent.target ):
                 # if it is completed, pursue a new one
                 if(t.completed):
-                    agent.target = choose_target(env,agent)
+                    agent.target = choose_target(env,agent,mode)
                     target_pos = agent.target_position
                 # else, keep going
                 else:
@@ -31,7 +31,7 @@ def c1_planning(env,agent):
 
     # else, choose one
     else:
-        agent.target = choose_target(env,agent)
+        agent.target = choose_target(env,agent,mode)
 
         # if did not find a valid target, move randomly
         if(not agent.target):
@@ -44,9 +44,24 @@ def c1_planning(env,agent):
 
             return action,agent.target
 
-def choose_target(env, agent):
-    for task in env.components['tasks']:
-        if int(task.index) % 2 == 1:
-            agent.target_position = task.position
-            return task.index
+def choose_target(env, agent, mode):
+    if mode == 'spatial':
+        # furthest task
+        distance = [np.sqrt((task.position[0] - agent.position[0])**2) +\
+         np.sqrt((task.position[1] - agent.position[1])**2) for task in env.components['tasks']]
+
+        if len(distance) == 0:
+            return None
+            
+        task_id = distance.index(max(distance))
+        agent.target_position = env.components['tasks'][task_id].position
+        return env.components['tasks'][task_id].index
+    elif mode == 'index':
+        # odd index
+        for task in env.components['tasks']:
+            if int(task.index) % 2 == 1:
+                agent.target_position = task.position
+                return task.index
+    else:
+        raise NotImplemented
     return None
--- src/reasoning/c2.py
+++ src/reasoning/c2.py
@@ -2,8 +2,8 @@ from a_star import a_star_planning
 import numpy as np
 import random
 
-# Pursues even index preys
-def c2_planning(env,agent):
+# Pursues even index/highest coordinates sum preys
+def c2_planning(env,agent,mode='spatial'):
     action, target_pos = None, None
     
     # checking if the agent already chosen a prey
@@ -12,7 +12,7 @@ def c2_planning(env,agent):
             if(t.index == agent.target ):
                 # if it is completed, pursue a new one
                 if(t.completed):
-                    agent.target = choose_target(env,agent)
+                    agent.target = choose_target(env,agent,mode)
                     target_pos = agent.target_position
                 # else, keep going
                 else:
@@ -31,7 +31,7 @@ def c2_planning(env,agent):
 
     # else, choose one
     else:
-        agent.target = choose_target(env,agent)
+        agent.target = choose_target(env,agent,mode)
 
         # if did not find a valid target, move randomly
         if(not agent.target):
@@ -44,9 +44,23 @@ def c2_planning(env,agent):
 
             return action,agent.target
 
-def choose_target(env, agent):
-    for task in env.components['tasks']:
-        if int(task.index) % 2 == 0:
-            agent.target_position = task.position
-            return task.index
+def choose_target(env, agent, mode):
+    if mode == 'spatial':
+        # highest coordinates sum task
+        coordinates_sum = [np.sum(task.position) for task in env.components['tasks']]
+
+        if len(coordinates_sum) == 0:
+            return None
+            
+        task_id = coordinates_sum.index(max(coordinates_sum))
+        agent.target_position = env.components['tasks'][task_id].position
+        return env.components['tasks'][task_id].index
+    elif mode == 'index':
+        # even index
+        for task in env.components['tasks']:
+            if int(task.index) % 2 == 0:
+                agent.target_position = task.position
+                return task.index
+    else:
+        raise NotImplemented
     return None
