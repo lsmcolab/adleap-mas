cfe4a6ac82c92b78111bfaf508a751b737b545af
Micanga <matheus.moog@gmail.com>
2021-08-29 10:57:16 -0300

    setting the wrong type experiments

--- scenario_generator.py
+++ scenario_generator.py
@@ -43,7 +43,7 @@ def get_initial_positions(env_name, dim, nagents, ntasks):
 
 def get_env_types(env_name):
     if env_name == "LevelForagingEnv":
-        return ['l1', 'l2', 'l3', 'l4', 'l5', 'l6']
+        return ['l1', 'l2']#, 'l3', 'l4', 'l5', 'l6']
     elif env_name == "CaptureEnv":
         return ['c1', 'c2'] # , 'c3'
     else:
@@ -92,7 +92,8 @@ def create_LevelForagingEnv(dim, num_agents, num_tasks, partial_observable=False
     from src.envs.LevelForagingEnv import LevelForagingEnv, Agent, Task
 
     # 2. Defining the types and directions of the environment
-    types = get_env_types("LevelForagingEnv")
+    #types = get_env_types("LevelForagingEnv")
+    types =  ['l3', 'l4']
     direction = [0, np.pi / 2, np.pi, 3 * np.pi / 2]
 
     # 3. Getting the initial positions
