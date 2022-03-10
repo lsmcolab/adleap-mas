78922ce31347a23de4edec734abd6c27b9cae7fd
Micanga <matheus.moog@gmail.com>
2022-03-10 12:31:07 -0300

    organising the reasoning folder

--- src/reasoning/levelbased/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.reasoning.levelbased import l1,l2,l3,l4,l5,l6
--- src/reasoning/t1.py
+++
@@ -1,9 +0,0 @@
-#####
-# TRUCO 1 ALGORITHM
-#####
-# returns a random action
-def t1_planning(state, agent):
-    action = state.action_space.sample()
-    while None in state.components['player'][state.current_player].hand[action]:
-        action = state.action_space.sample()
-    return action, None
